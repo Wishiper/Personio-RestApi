3f3cfc190e4a8aeb77aacd8442dbdd182599043d
Aleksandar_Georgiev <Aleksandar_Georgiev@epam.com>
2021-11-23 17:40:57 +0200

    Test commit develop branch

--- Hierarchy/src/main/java/com/Personio/Hierarchy/HierarchyApplication.java
+++ Hierarchy/src/main/java/com/Personio/Hierarchy/HierarchyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HierarchyApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting Hierarchy Project ...");
 		SpringApplication.run(HierarchyApplication.class, args);
 	}
 
